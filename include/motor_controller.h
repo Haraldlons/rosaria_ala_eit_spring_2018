d676e5923cf7499ae3ed0df465d49a1978dc4249
Harald Lønsethagen <haraldlons@gmail.com>
2018-03-21 10:28:59 +0100

    include header file and update CMakeList

--- include/motor_controller.h
+++ include/motor_controller.h
@@ -1,16 +1,16 @@
 #include <ros/ros.h>
 
 class Motor_Controller {
-public:
-	Motor_Controller();
 private:
-	setSteeringCommand(nav_msgs::Odometry&);
 
 	double integral_ = 0;
 	double T_i_ = 5;
-	double K_p_ = 1;
-
+	double K_p_ = 6;
 	ros::NodeHandle nh_;
 	ros::Subscriber pose_sub_;
 	ros::Publisher cmd_vel_pub_;
-}
+	ros::Publisher yaw_;
+	void setSteeringCommand(nav_msgs::Odometry::ConstPtr);
+public:
+	Motor_Controller();
+};
